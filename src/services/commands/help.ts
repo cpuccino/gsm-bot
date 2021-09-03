09b60473c245d10786a03a079071acf67d583ab7
cpuccino <thegreatcappuccino@gmail.com>
2021-09-03 23:53:25 +1000

    format: Format help.ts

--- src/services/commands/help.ts
+++ src/services/commands/help.ts
@@ -2,7 +2,10 @@ import { Client, Command, CommandMessage, Description } from '@typeit/discord';
 import table from 'text-table';
 
 function generateCommandTable(): string {
-  const columns = Client.getCommands().map(({ commandName, description }) => [commandName, description]);
+  const columns = Client.getCommands().map(({ commandName, description }) => [
+    commandName,
+    description
+  ]);
   return table([...columns]);
 }
 
