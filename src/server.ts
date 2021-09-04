cfa655606f91faa735036f2045d6bdfe2ad7a3cb
cpuccino <thegreatcappuccino@gmail.com>
2021-09-04 18:29:36 +1000

    chore: Updated server main function

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,15 @@
 import 'reflect-metadata';
 
-// import path from 'path';
-
-// import { Client } from '@typeit/discord';
-// import { GSM_BOT_TOKEN } from './configuration/environment';
-
-// export async function main() {
-//   const client = new Client();
-//   await client.login(GSM_BOT_TOKEN, path.join(__dirname, 'discords', '*.[jt]s'));
-
-//   console.log(Client.getCommands());
-// }
-
-// main();
+import path from 'path';
 
 import { Client } from '@typeit/discord';
 import { GSM_BOT_TOKEN } from './configuration/environment';
 
-export class Server {
-  private static client: Client;
+export async function main() {
+  const client = new Client();
+  await client.login(GSM_BOT_TOKEN, path.join(__dirname, 'discords', '*.[jt]s'));
 
-  static get Client(): Client {
-    return this.client;
-  }
-
-  static start(): void {
-    this.client = new Client();
-    this.client.login(GSM_BOT_TOKEN, `${__dirname}/discords/*.[jt]s`);
-    console.log(Client.getCommands());
-  }
+  console.log(Client.getCommands());
 }
 
-Server.start();
+main();
